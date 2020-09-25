5d1b81e20e9d6de9a653da17c9d6bc99d641c78c
Christian marucco <christian.marucco@devlights.com>
2020-09-25 20:13:48 -0300

    add setting for sass and git ignore

---
+++ components/header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../../styles/header.scss"
+
+function Header (){
+
+    return(
+         
+        <div className=" header ">
+        soy una barra de navegacion
+        </div>
+
+
+    );
+}
+export default Header;
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/index.scss'
+import Header from "../components/header/Header"
+
+export default function Index () {
+     return (
+
+
+        <div className="app"> 
+            <Header></Header>
+             </div>
+
+     )
+     
+}
